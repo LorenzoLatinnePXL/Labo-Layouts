611c929a7263abb65ad9aa61888e2073df5ae112
LorenzoLatinnePXL <11701160@student.pxl.be>
2025-09-30 16:18:08 +0200

    Labo finished.

--- Layouts/MainPage.xaml.cs
+++ Layouts/MainPage.xaml.cs
@@ -2,13 +2,15 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
         }
 
+        private async void GridPageButtonClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync(nameof(GridPage));
+        }
     }
 
 }
